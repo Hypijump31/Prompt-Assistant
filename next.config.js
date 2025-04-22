9e2716fda53dba58068811329809bafce7af6078
Hypijump31 <hypijump@gmail.com>
2025-04-23 01:53:14 +0200

    ajout gh page

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: 'export',
   // Pour GitHub Pages, il est recommandé de définir le chemin de base si le repo n'est pas à la racine du user
-  // basePath: '/NOM_DU_REPO',
-  // assetPrefix: '/NOM_DU_REPO/',
+  basePath: '/Prompt-Assistant',
+  assetPrefix: '/Prompt-Assistant/',
 };
 
 module.exports = nextConfig;
