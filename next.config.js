b9c5075d5580578ee0d06c9fd0a16e0ac531b75c
Hypijump31 <hypijump@gmail.com>
2025-04-23 01:08:05 +0200

    ajout github page

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Pour GitHub Pages, il est recommandé de définir le chemin de base si le repo n'est pas à la racine du user
+  // basePath: '/NOM_DU_REPO',
+  // assetPrefix: '/NOM_DU_REPO/',
+};
+
+module.exports = nextConfig;
