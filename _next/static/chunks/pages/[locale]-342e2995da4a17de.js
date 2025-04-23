a7fe9618d6a329894362aad0841f39c26e748e29
Hypijump31 <Hypijump31@users.noreply.github.com>
2025-04-23 02:18:57 +0000

    deploy: aeded71a1d6a3042dda064cc86a3093e23b69716

---
+++ _next/static/chunks/pages/[locale]-342e2995da4a17de.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[422],{5779:(_,e,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/[locale]",function(){return u(7296)}])},7296:(_,e,u)=>{"use strict";u.r(e),u.d(e,{__N_SSG:()=>l,default:()=>n});var a=u(3757),l=!0;let n=a.default}},_=>{var e=e=>_(_.s=e);_.O(0,[949,757,636,593,792],()=>e(5779)),_N_E=_.O()}]);
